27d0af544f358c24ff15d286cbb9220a74fa0a31
Unknown <roth@pscouncil.org>
2019-04-26 15:42:45 -0400

    42619

--- Cleaning Agency Data for Top PSC Charts.R
+++ Cleaning Agency Data for Top PSC Charts.R
@@ -6,20 +6,24 @@ library(tidyverse)
 
 #Location for saving charts
 setwd("~/Vision/2019/All top PSC Graphs")
-
-data <- read_csv("S:/1 Marielle Folder/Data Sets/By Agency/HHS/HHS FY18.csv") ###
+####load all data (From BGOV) for an agency for the most current full year; if larger data set (not BGOV limitations) load for full year)
+data <- read_csv("X:/1 Marielle Folder/Data Sets/By Agency/HHS/HHS FY18.csv") ###location
 dpap <- read_csv("~/Reference Tables/DPAP Crosswalk.csv")
 
+##Filter if Sub-Agency; if larger dataset, divide into lists by agency and bureau, run lapply for quicker result
 top_psc <- data %>% 
   rename(PSC = 'Product Service Code (PSC) / Federal Supply Code (FSC)',
          transaction_value = 'Transaction Value',
          fiscal_year = "Fiscal Year") %>% 
+#filter(`Funding Bureau` == "Centers for Medicare and Medicaid Services (CMS)") %>% 
+#  filter(`Funding Bureau` == "National Institutes of Health (NIH)") %>%   
+  filter(`Funding Bureau` == "Food and Drug Administration (FDA)") %>% 
 #  filter(`Funding Bureau`== "Bureau of Alcohol, Tobacco, Firearms and Explosives (ATF)") %>% 
 #  filter(`Funding Bureau`== "Drug Enforcement Administration (DEA)") %>% 
 #  filter(`Funding Bureau`== "Federal Bureau of Investigation (FBI)") %>% 
 #  filter(`Funding Bureau`== "Justice Management Division (JMD)") %>% 
 #  filter(`Funding Bureau`== "United States Marshals Service (USMS)") %>% 
-  filter(`Funding Bureau`== "Food and Drug Administration (FDA)") %>%
+#  filter(`Funding Bureau`== "Food and Drug Administration (FDA)") %>%
   #filter(fiscal_year == 2018) %>% 
   select(PSC, transaction_value, fiscal_year) %>% 
   left_join(select(dpap, c('PSC Code', 'PSC Description', 'P.S')), by = c(PSC = 'PSC Code')) %>% 
@@ -33,7 +37,7 @@ top_psc <- data %>%
 top_psc$fiscal_year <- as.character(top_psc$fiscal_year)
 
 
-write.csv(top_psc, "FDA FY18 top psc.csv")              ###
+write.csv(top_psc, "FDA FY18 top psc.csv")              ###name new file
 
 
-##Top PSC by Sub-Agency####
+
